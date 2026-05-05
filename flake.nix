6aa21bb7fd59ee53ee17556828eeb597b06a009d
Jeremy Diamond <jeremygdiamond@proton.me>
2026-05-05 15:02:49 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "flake with zig master (0.16-dev)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    zig.url = "github:mitchellh/zig-overlay";
+  };
+
+  outputs = { self, nixpkgs, zig, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        zig.packages.${system}.master
+      ];
+      shellHook = ''echo "Using zig: $(zig version)"'';
+    };
+  };
+}
